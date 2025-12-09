c70c90c2f450ceec1af02a49484c4a4fd3989edd
Chris <chrissywestphal1@gmail.com>
2025-12-09 07:52:08 +0100

    Initial commit: add password generator script and .gitignore

---
+++ main.py
@@ -0,0 +1,29 @@
+
+import tkinter as tk
+import random
+import string
+
+def generate_passwort():
+    password = ""
+    for i in range(12):
+        password += random.choice(string.ascii_letters)
+        label.config(text=password)
+
+root = tk.Tk()
+root.title("Password Generator")
+root.geometry("500x500")
+
+label = tk.Label(root, text="Hier erscheint das Passwort")
+label.pack()
+
+button = tk.Button(root, text="Passwort generieren", command=generate_passwort)
+button.pack()
+
+
+
+
+
+
+
+
+root.mainloop()
